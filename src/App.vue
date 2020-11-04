01699399beb819c65d67f8cd594f8bf610468a28
Aisen60 <caixusheng60@gmail.com>
2020-11-04 23:41:57 +0800

    chore: update author

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@
   </section>
   <footer class="info">
     <p>Double-click to edit a todo</p>
-    <p>Created by <a href="http://sindresorhus.com">Aisen60</a></p>
+    <p>Created by <a href="https://github.com/Aisen60">Aisen60</a></p>
     <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
   </footer>
 </template>
