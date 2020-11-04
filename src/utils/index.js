f30bc95ddaa3fbfdd95297edbc6559dbe6fd2d79
Aisen60 <caixusheng60@gmail.com>
2020-11-04 23:38:57 +0800

    refactor: update index.js to useLocalStorage.js

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const getStorage = (key) => {
-  return window.localStorage.getItem(key);
-};
-
-export const setStorage = (key, value) => {
-  return window.localStorage.setItem(key, value);
-};
